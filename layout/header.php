82348f0857da73481956b69ed70ac15870ce3b07
Ferhat Cengiz <fc@aof.anadolu.edu.tr>
2024-09-15 05:39:39 +0300

    Refactor landing page: Update link to view all products

--- layout/header.php
+++ layout/header.php
@@ -1,27 +1,45 @@
 <header id="header" class="header d-flex align-items-center">
-    <div id="navbarBg"
-        class="fixed-top mt-4 mb-4 container-fluid container-xl d-flex align-items-center justify-content-between">
+  <div id="navbarBg"
+    class="fixed-top mt-4 mb-4 container-fluid container-xl d-flex align-items-center justify-content-between">
 
-        <a href="index.html" class="logo d-flex align-items-center">
-            <!-- Uncomment the line below if you also wish to use an image logo -->
-            <!-- <img src="assets/img/logo.png" alt=""> -->
-            <p style="font-size: 24px; font-weight: 700; color: #fff; margin-bottom: 0; font-family: var(--font-primary);">
-                Bayrak Oto Lastik<span>
-                    <span class="text-warning">.</span>
-                </span></p>
-        </a>
+    <a href="index.html" class="logo d-flex align-items-center">
+      <!-- Uncomment the line below if you also wish to use an image logo -->
+      <!-- <img src="assets/img/logo.png" alt=""> -->
+      <p style="font-size: 24px; font-weight: 700; color: #fff; margin-bottom: 0; font-family: var(--font-primary);">
+        Bayrak Oto Lastik<span>
+          <span class="text-warning">.</span>
+        </span></p>
+    </a>
 
-        <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
-        <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
-        <nav id="navbar" class="navbar">
-            <ul>
-                <li><a href="./#get-started">İletişim</a></li>
-                <li><a href="./#services">Hizmetler</a></li>
-                <li><a href="./#alt-services">Hakkımızda</a></li>
-                <li><a href="./#faq">S.S.S</a></li>
-                <li><a href="./#projects">Öne Çıkan Ürünler</a></li>
-                <li><a href="./magaza">Tüm Ürünler</a></li>
-                <!-- <li class="dropdown"><a href="#"><span>Dropdown</span> <i
+    <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+    <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+    <nav id="navbar" class="navbar">
+      <ul>
+        <li><a href="./#get-started">İletişim</a></li>
+        <li><a href="./#services">Hizmetler</a></li>
+        <li><a href="./#alt-services">Hakkımızda</a></li>
+        <li><a href="./#faq">S.S.S</a></li>
+        <li><a href="./#projects">Öne Çıkan Ürünler</a></li>
+        <!-- <li><a href="./magaza">Tüm Ürünler</a></li> -->
+
+        <li class="dropdown"><a href="#"><span>Tüm Ürünler</span> <i
+              class="bi bi-chevron-down dropdown-indicator"></i></a>
+          <ul>
+
+            <li><a href="./magaza">
+                Lastik Ebatına Göre
+              </a></li>
+            <li><a href="./magaza">
+                Araç Modeline Göre
+              </a></li>
+            <li><a href="./magaza/urunler">
+                Tüm Ürünleri Gör
+              </a></li>
+          </ul>
+        </li>
+
+
+        <!-- <li class="dropdown"><a href="#"><span>Dropdown</span> <i
                 class="bi bi-chevron-down dropdown-indicator"></i></a>
             <ul>
               <li><a href="#">Dropdown 1</a></li>
@@ -41,8 +59,8 @@
             </ul>
           </li>
           <li><a href="contact.html">Contact</a></li> -->
-            </ul>
-        </nav><!-- .navbar -->
+      </ul>
+    </nav><!-- .navbar -->
 
-    </div>
+  </div>
 </header><!-- End Header -->
